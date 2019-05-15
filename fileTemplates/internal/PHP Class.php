028939d5fa82f27f729ae45e9b0c800548990152
collinsmao <collinsmao@foxmail.com>
2019-05-15 22:23:53 +0800

    AI-3.4 <Collins@Collins Create PHP Class.php, IntelliLang.xml, project.default.xml, diff.xml, Enum.java, Interface.java, Default.xml, AngularJS .xml, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, Visual Studio copy.xml, androidEditors.xml, PHP Implemented Method Body.php, Angular2.xml, Zen HTML.xml, abbreviations.xml, Default.xml, PHP Overridden Method Body.php, Zen XSL.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, Singleton.java, other.xml, Zen CSS.xml, Twig.xml, androidStudioFirstRun.xml, ui.lnf.xml, filetypes.xml, Default copy.icls, remote-server-client-libraries.xml, notifications.xml, templates.xml, AnnotationType.java, studioFlags.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, web-browsers.xml, proxy.settings.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,14 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+/**
+ * Class ${NAME}
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ */
+class ${NAME} {
+
+}
